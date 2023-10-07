1b628ccc17338b21fd89caccde3befebf1943cf5
DrealVeerNanda <68409762+DrealVeerNanda@users.noreply.github.com>
2023-10-06 22:44:24 -0700

    Localizer
    Localizer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TwoDeadWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/TwoDeadWheelLocalizer.java
@@ -23,6 +23,9 @@ public final class TwoDeadWheelLocalizer implements Localizer {
     public static class Params {
         public double parYTicks = 0.0; // y position of the parallel encoder (in tick units)
         public double perpXTicks = 0.0; // x position of the perpendicular encoder (in tick units)
+        
+        // offsets
+
     }
 
     public static Params PARAMS = new Params();
@@ -95,4 +98,5 @@ public final class TwoDeadWheelLocalizer implements Localizer {
 
         return twist;
     }
+    //Localizer update
 }
