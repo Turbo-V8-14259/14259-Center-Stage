3bcf36400ebe274586e3b9268404efb880ed5983
DrealVeerNanda <68409762+DrealVeerNanda@users.noreply.github.com>
2023-10-07 18:46:09 -0700

    fix
    fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/usefuls/Sensors/axonEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/usefuls/Sensors/axonEncoder.java
@@ -5,23 +5,18 @@ import com.qualcomm.robotcore.hardware.AnalogInput;
 public class axonEncoder{
 
     private AnalogInput analog;
-    private double voltage;
 
     private double angle = 0;
 
     public axonEncoder(AnalogInput analog) {
         this.analog = analog;
-        this.voltage = getVoltage();
     }
 
-
-    public double getVoltage() {
-        voltage = this.analog.getVoltage();
-        return 0;
+    public double getVoltage(){
+        return analog.getVoltage();
     }
-
     public double update() {
-        angle = voltage/3.3*360;
+        angle = getVoltage()/3.3*360;
         return angle;
     }
 }
