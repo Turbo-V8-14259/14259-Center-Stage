9c60107279e86f97318e326559b02f40e7051587
DrealVeerNanda <68409762+DrealVeerNanda@users.noreply.github.com>
2023-10-12 10:14:05 -0700

    Intake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Intake.java
@@ -1,4 +1,30 @@
-package org.firstinspires.ftc.teamcode.hardware;
+package org.firstinspires.ftc.teamcode.hardware.Deposit;
 
-public class Intake {
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.usefuls.Math.M;
+import org.firstinspires.ftc.teamcode.usefuls.Motor.DcMotorBetter;
+import org.firstinspires.ftc.teamcode.usefuls.Motor.PID;
+
+public class Intake{
+    public double intakePower = 0;
+    public DcMotorEx intakeMotor;
+
+    public Intake(DcMotorEx intakeMotor) {
+        this.intakeMotor = intakeMotor;
+
+    }
+
+    public Intake stop(){
+        this.intakeMotor.setPower(0);
+        intakePower = 0;
+        return this;
+    }
+    public boolean isBusy() { return this.intakeMotor.isBusy();}
+
+    public void update() {
+        this.intakeMotor.setPower(intakePower);
+    }
 }
