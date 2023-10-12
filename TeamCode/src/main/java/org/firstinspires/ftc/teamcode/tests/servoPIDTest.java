de7f60fed7ea8093293609ea60b1be195656a8da
DrealVeerNanda <68409762+DrealVeerNanda@users.noreply.github.com>
2023-10-12 09:59:29 -0700

    Radians

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/servoPIDTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/servoPIDTest.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.tests;
 
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.AnalogInput;
@@ -8,18 +9,16 @@ import com.qualcomm.robotcore.hardware.CRServo;
 
 import org.firstinspires.ftc.teamcode.usefuls.Math.M;
 import org.firstinspires.ftc.teamcode.usefuls.Motor.AnglePID;
-import org.firstinspires.ftc.teamcode.usefuls.Motor.PID;
 import org.firstinspires.ftc.teamcode.usefuls.Sensors.axonEncoder;
 @TeleOp
+@Config
 public class servoPIDTest extends LinearOpMode {
     private axonEncoder angle;
     private AnalogInput angularEncoder;
     private CRServo a;
     private AnglePID servoController;
     private double Kp = 0, Ki = 0, Kd = 0;
-
     private double targetServoAngle = 0;
-
     private double angleInRad = 0;
 
     @Override
