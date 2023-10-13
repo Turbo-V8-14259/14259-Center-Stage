8255533c21394aba545436a27af1c2c3d4555c53
DrealVeerNanda <68409762+DrealVeerNanda@users.noreply.github.com>
2023-10-12 23:45:42 -0700

    Dooplicate Delete

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Sensors/Blinkdin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Sensors/Blinkdin.java
@@ -1,4 +1,22 @@
 package org.firstinspires.ftc.teamcode.hardware.Sensors;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 
 public class Blinkdin {
+
+    public RevBlinkinLedDriver led;
+
+    public RevBlinkinLedDriver.BlinkinPattern pattern;
+
+    public Blinkdin(RevBlinkinLedDriver led){
+        this.led = led;
+    }
+
+    public Blinkdin changePattern(RevBlinkinLedDriver.BlinkinPattern newPattern){
+        this.pattern = newPattern;
+    }
+
+    public void update(){
+        this.led.setPattern(pattern);
+    }
+
 }
