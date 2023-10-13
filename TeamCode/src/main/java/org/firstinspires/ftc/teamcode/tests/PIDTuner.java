68c8316a779a84996319fc9b57ec0683d23b6317
DrealVeerNanda <68409762+DrealVeerNanda@users.noreply.github.com>
2023-10-13 09:52:26 -0700

    PID

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/PIDTuner.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Config
+@TeleOp
+public class PIDTuner {
+}
