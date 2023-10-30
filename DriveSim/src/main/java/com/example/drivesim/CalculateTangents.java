843515fddec6bb1e4921ca730e9f322c115e1a11
DrealVeerNanda <68409762+DrealVeerNanda@users.noreply.github.com>
2023-10-30 00:08:47 -0700

    DriveSim update + forgor battery at garage so no testing

--- DriveSim/src/main/java/com/example/drivesim/CalculateTangents.java
+++ DriveSim/src/main/java/com/example/drivesim/CalculateTangents.java
@@ -1,27 +1,27 @@
 package com.example.drivesim;
 
-import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 
 public class CalculateTangents {
     public static double calculateTangent(Pose2d initialPosition, Pose2d finalPosition) {
-        double xd = initialPosition.getX() - finalPosition.getX();
-        double yd = initialPosition.getY() - finalPosition.getY();
+        double xd = initialPosition.position.x - finalPosition.position.x;
+        double yd = initialPosition.position.y - finalPosition.position.y;
         return Math.atan2(yd,xd) - Math.PI;
     }
     public static double calculateTangent(Vector2d initialPosition, Vector2d finalPosition) {
-        double xd = initialPosition.getX() - finalPosition.getX();
-        double yd = initialPosition.getY() - finalPosition.getY();
+        double xd = initialPosition.x - finalPosition.x;
+        double yd = initialPosition.y - finalPosition.y;
         return Math.atan2(yd,xd) - Math.PI;
     }
     public static double calculateTangent(Pose2d initialPosition, Vector2d finalPosition) {
-        double xd = initialPosition.getX() - finalPosition.getX();
-        double yd = initialPosition.getY() - finalPosition.getY();
+        double xd = initialPosition.position.x - finalPosition.x;
+        double yd = initialPosition.position.y - finalPosition.y;
         return Math.atan2(yd,xd) - Math.PI;
     }
     public static double calculateTangent(Vector2d initialPosition, Pose2d finalPosition) {
-        double xd = initialPosition.getX() - finalPosition.getX();
-        double yd = initialPosition.getY() - finalPosition.getY();
+        double xd = initialPosition.x - finalPosition.position.x;
+        double yd = initialPosition.y - finalPosition.position.y;
         return Math.atan2(yd,xd) - Math.PI;
     }
 }
