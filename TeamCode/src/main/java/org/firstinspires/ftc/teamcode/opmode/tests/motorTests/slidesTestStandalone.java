d7262ae17a44256a54fe172d1e1c9c9c6d40866c
DrealVeerNanda <68409762+DrealVeerNanda@users.noreply.github.com>
2023-11-01 16:35:01 -0700

    Slides

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tests/motorTests/slidesTestStandalone.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.opmode.tests.motorTests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+@TeleOp
+public class slidesTestStandalone extends LinearOpMode {
+
+    double power = 0;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotorEx slidesLeft = hardwareMap.get(DcMotorEx.class, "leftSlides");
+        DcMotorEx slidesRight = hardwareMap.get(DcMotorEx.class, "rightSlides");
+
+        slidesRight.setDirection(DcMotorEx.Direction.FORWARD);
+        slidesRight.setDirection(DcMotorEx.Direction.FORWARD);
+        //change to reverse if needed
+        waitForStart();
+        while(opModeIsActive()){
+            power = gamepad1.left_trigger - gamepad1.right_trigger;
+            slidesRight.setPower(power);
+            slidesLeft.setPower(power);
+        }
+    }
+}
