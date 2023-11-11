cd5dd2a822512568c3b99adc890a0f231739c82b
DrealVeerNanda <68409762+DrealVeerNanda@users.noreply.github.com>
2023-11-10 16:08:57 -0800

    teleop/config

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tests/oldIMU.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tests/oldIMU.java
@@ -1,7 +1,11 @@
 package org.firstinspires.ftc.teamcode.opmode.tests;
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@TeleOp
+@Config
 public class oldIMU extends LinearOpMode {
     private BNO055IMU imu;
     @Override
