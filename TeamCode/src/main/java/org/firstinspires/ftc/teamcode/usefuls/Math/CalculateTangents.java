08605ab62cb50b02ea5c62283f245580331529e8
DrealVeerNanda <68409762+DrealVeerNanda@users.noreply.github.com>
2023-11-15 20:18:54 -0800

    refactor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/usefuls/Math/CalculateTangents.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/usefuls/Math/CalculateTangents.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode.usefuls.Math;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
