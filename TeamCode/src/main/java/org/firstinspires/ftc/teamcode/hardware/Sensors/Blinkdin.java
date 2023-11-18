7b7a974e8213fbdbf660c9234485acbd5826ada7
DrealVeerNanda <68409762+DrealVeerNanda@users.noreply.github.com>
2023-11-17 17:54:42 -0800

    sm sm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Sensors/Blinkdin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Sensors/Blinkdin.java
@@ -1,23 +1,31 @@
 package org.firstinspires.ftc.teamcode.hardware.Sensors;
 import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.hardware.Deposit.DepoArm;
+import org.firstinspires.ftc.teamcode.usefuls.Motor.ServoMotorBetter;
 
 public class Blinkdin {
 
-    public RevBlinkinLedDriver led;
+    double target = 0;
+    ServoMotorBetter led;
 
     public RevBlinkinLedDriver.BlinkinPattern pattern;
 
-    public Blinkdin(RevBlinkinLedDriver led){
+    public Blinkdin(ServoMotorBetter led){
         this.led = led;
+        this.led.setLowerBound(0.2525);
+        this.led.setUpperBound(.7475);
     }
 
-    public Blinkdin changePattern(RevBlinkinLedDriver.BlinkinPattern newPattern){
-        this.pattern = newPattern;
+    public Blinkdin changePattern(double setting){
+        this.target = setting;
         return this;
     }
 
     public void update(){
-        this.led.setPattern(pattern);
+        this.led.setPosition(target);
+        this.led.update();
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tests/blinkdinTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tests/blinkdinTest.java
@@ -3,9 +3,10 @@ package org.firstinspires.ftc.teamcode.opmode.tests;
 import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.hardware.Sensors.Blinkdin;
-
+import org.firstinspires.ftc.teamcode.usefuls.Motor.ServoMotorBetter;
 
 
 @TeleOp
@@ -13,14 +14,9 @@ public class blinkdinTest extends LinearOpMode {
     Blinkdin led;
     @Override
     public void runOpMode() throws InterruptedException {
-        led = new Blinkdin(hardwareMap.get(RevBlinkinLedDriver.class, "led"));
-
-        while(opModeInInit()){
-            led.changePattern(RevBlinkinLedDriver.BlinkinPattern.WHITE);
-            led.update();
-        }
+        led = new Blinkdin(new ServoMotorBetter(hardwareMap.get(Servo.class, "led")));
         while(opModeIsActive()){
-            led.changePattern(RevBlinkinLedDriver.BlinkinPattern.RED);
+            led.changePattern(gamepad1.left_trigger);
             led.update();
         }
 
