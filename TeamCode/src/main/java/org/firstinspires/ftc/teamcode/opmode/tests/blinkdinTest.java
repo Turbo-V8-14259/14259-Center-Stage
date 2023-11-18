a013ddb913d249f8f148b1ba8d4f563d13711b36
DrealVeerNanda <68409762+DrealVeerNanda@users.noreply.github.com>
2023-11-17 17:28:19 -0800

    blinkdin

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tests/blinkdinTest.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.opmode.tests;
+
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.hardware.Sensors.Blinkdin;
+
+
+
+@TeleOp
+public class blinkdinTest extends LinearOpMode {
+    Blinkdin led;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        led = new Blinkdin(hardwareMap.get(RevBlinkinLedDriver.class, "led"));
+
+        while(opModeInInit()){
+            led.changePattern(RevBlinkinLedDriver.BlinkinPattern.WHITE);
+            led.update();
+        }
+        while(opModeIsActive()){
+            led.changePattern(RevBlinkinLedDriver.BlinkinPattern.RED);
+            led.update();
+        }
+
+    }
+}
