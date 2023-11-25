010b5a33203585d9e65058d791a42bc6957e5043
momo0222 <joywang0222@gmail.com>
2023-11-25 14:33:53 -0800

    rrauto

--- FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
+++ FtcRobotController/build/generated/source/buildConfig/debug/com/qualcomm/ftcrobotcontroller/BuildConfig.java
@@ -8,9 +8,5 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "com.qualcomm.ftcrobotcontroller";
   public static final String BUILD_TYPE = "debug";
   // Field from default config.
-
-  public static final String APP_BUILD_TIME = "2023-11-21T17:58:03.794-0800";
-
-
-
+  public static final String APP_BUILD_TIME = "2023-11-21T20:56:32.497-0800";
 }
