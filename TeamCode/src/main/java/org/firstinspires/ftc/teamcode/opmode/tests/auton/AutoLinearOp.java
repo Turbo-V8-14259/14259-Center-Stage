ec9c4d8c10955a4dfe7e9fa208c200256367e902
momo0222 <joywang0222@gmail.com>
2023-11-25 15:11:57 -0800

    auto test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tests/auton/AutoLinearOp.java
@@ -0,0 +1,122 @@
+package org.firstinspires.ftc.teamcode.opmode.tests.auton;
+
+import static org.firstinspires.ftc.teamcode.vision.Red.Location.LEFT;
+import static org.firstinspires.ftc.teamcode.vision.Red.Location.MIDDLE;
+import static org.firstinspires.ftc.teamcode.vision.Red.Location.RIGHT;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.hardware.Deposit.DepoArm;
+import org.firstinspires.ftc.teamcode.hardware.Deposit.DepoSlides;
+import org.firstinspires.ftc.teamcode.hardware.Deposit.LM1Turret;
+import org.firstinspires.ftc.teamcode.hardware.Intake.Intake;
+import org.firstinspires.ftc.teamcode.usefuls.Motor.DcMotorBetter;
+import org.firstinspires.ftc.teamcode.usefuls.Motor.ServoMotorBetter;
+import org.firstinspires.ftc.teamcode.vision.Red;
+import org.firstinspires.ftc.vision.VisionPortal;
+
+@Autonomous
+public class AutoLinearOp extends LinearOpMode {
+
+    enum State{
+        IDLE,
+        TEAMPROP,
+        INTAKE,
+        TRAVEL,
+        STACK, //picks up pixels from stack
+        BOARD, //to scoring board
+        SCORE //arm scores
+    }
+    private Red.Location location = Red.Location.MIDDLE;
+    private Red redPropProcessor;
+    private VisionPortal visionPortal;
+    int randomization = 2;
+    //0 means left, 1 means middle, 2 means right.
+    SampleMecanumDrive drive;
+
+    Intake intake;
+
+    LM1Turret turret;
+    DepoArm arm;
+    DepoSlides slides;
+
+    State currentstate = State.IDLE;
+
+    Pose2d startPose = new Pose2d(-35, 65, Math.toRadians(90));
+    Pose2d initialLine = new Pose2d(-35, -15, Math.toRadians(-90));
+
+    Pose2d leftProp = new Pose2d(-42, -32, Math.toRadians(-90));
+    Pose2d rightProp = new Pose2d(-32, -40, Math.toRadians(0));
+    Pose2d middleProp = new Pose2d(-30, -21, Math.toRadians(-90));
+    Pose2d prop[] = {leftProp, middleProp, rightProp};
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        drive = new SampleMecanumDrive(hardwareMap);
+        intake = new Intake(hardwareMap.get(DcMotorEx.class, "Intake"), new ServoMotorBetter(hardwareMap.get(Servo.class, "intakeArm")));
+        intake.setState(Intake.IntakeState.INITIALIZE);
+        intake.update();
+
+        turret = new LM1Turret(new ServoMotorBetter(hardwareMap.get(Servo.class, "turret")));
+
+        arm = new DepoArm(new ServoMotorBetter(hardwareMap.get(Servo.class, "arm")), new ServoMotorBetter(hardwareMap.get(Servo.class, "fake")));
+
+        slides = new DepoSlides(new DcMotorBetter(hardwareMap.get(DcMotorEx.class, "leftSlides")), new DcMotorBetter(hardwareMap.get(DcMotorEx.class, "rightSlides")));
+
+        slides.passive = false;
+        slides.pidRunning = true;
+        slides.manualMode = false;
+
+        drive.setPoseEstimate(startPose);
+
+        Trajectory trajectory1 = drive.trajectoryBuilder(startPose)
+                .lineToLinearHeading(initialLine)
+                .build();
+        Trajectory trajectory2 = drive.trajectoryBuilder(trajectory1.end())
+                .lineToLinearHeading(prop[randomization])
+                .build();
+
+        redPropProcessor = new Red(telemetry);
+        visionPortal = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class, "Webcam 1"), redPropProcessor);
+
+        while(!isStarted()){
+            location = redPropProcessor.getLocation();
+            if(location==LEFT){
+                randomization = 0;
+            }else if(location==MIDDLE){
+                randomization = 1;
+            }else if(location==RIGHT){
+                randomization = 2;
+            }
+            telemetry.update();
+        }
+        currentstate = State.TRAVEL;
+        drive.followTrajectoryAsync(trajectory1);
+
+        while (opModeIsActive() && !isStopRequested()){
+            switch(currentstate){
+                case TRAVEL:
+                    if(!drive.isBusy()){
+                        currentstate = State.TEAMPROP;
+                        drive.followTrajectoryAsync(trajectory2);
+                    }
+                case TEAMPROP:
+                    if(!drive.isBusy()) {
+                        intake.setState(Intake.IntakeState.AUTO_HIGH);
+                        intake.update();
+                        sleep(500);
+                    }
+            }
+        }
+
+
+    }
+}
