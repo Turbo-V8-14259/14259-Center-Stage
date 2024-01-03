ee549b4e85a5813f012ba2aa9cdc4dc72765d588
momo0222 <joywang0222@gmail.com>
2024-01-03 13:22:43 -0800

    refactor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/path/RobotController.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.drive.path;
+
+import org.opencv.core.Point;
+
+public class RobotController {
+    private Robot robot;
+
+    public RobotController(Robot robot){
+        this.robot = robot;
+    }
+    public void goTo(Point waypt1, Point waypt2, double followRadius, int index){
+        while(!PurePursuitUtil.passedWayPt(robot.getLocation(), waypt2, followRadius)){
+            Point follow = PurePursuitUtil.followMe(waypt1, waypt2, robot.getLocation(), followRadius);
+            robot.setPosition(follow);
+            robot.updateControl();
+        }
+        System.out.println("Robot passed waypoint "+ index+1);
+    }
+}
