19e9babb310e1e8a95095f6de16d0723f180e3b1
momo0222 <joywang0222@gmail.com>
2024-02-17 18:42:32 -0800

    sensors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Sensors/Blinkdin.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Sensors/Blinkdin.java
@@ -15,6 +15,21 @@ public class Blinkdin {
         return this;
     }
 
+    public Blinkdin changeColor(String color){
+        if(color.equals("YELLOW")){
+            target = RevBlinkinLedDriver.BlinkinPattern.YELLOW;
+        }else if(color.equals("GREEN")){
+            target = RevBlinkinLedDriver.BlinkinPattern.GREEN;
+        }else if(color.equals("PURPLE")){
+            target = RevBlinkinLedDriver.BlinkinPattern.VIOLET;
+        }else if(color.equals("WHITE")){
+            target = RevBlinkinLedDriver.BlinkinPattern.WHITE;
+        }else{
+            target = RevBlinkinLedDriver.BlinkinPattern.RED;
+        }
+        return this;
+    }
+
     public RevBlinkinLedDriver.BlinkinPattern getPattern(){
         return target;
     }
