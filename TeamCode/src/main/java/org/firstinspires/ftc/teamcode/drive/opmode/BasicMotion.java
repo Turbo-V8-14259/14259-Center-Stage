45975b7370c10e00bd291c5041611a9a31bac543
momo0222 <joywang0222@gmail.com>
2024-04-02 16:21:14 -0700

    testing angle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/BasicMotion.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/BasicMotion.java
@@ -6,9 +6,13 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.util.Timer;
 
 @TeleOp
 public class BasicMotion extends LinearOpMode {
+    final double SECSPERROTATION = 1200;
     @Override
     public void runOpMode() throws InterruptedException {
         // Declare our motors
@@ -18,6 +22,11 @@ public class BasicMotion extends LinearOpMode {
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRight");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("backRight");
 
+        double rx = 1;
+        double angle = 135;
+
+        double ratio = (angle/360)*SECSPERROTATION;
+        ElapsedTime timer = new ElapsedTime();
         // Reverse the right side motors. This may be wrong for your setup.
         // If your robot moves backwards when commanded to go forwards,
         // reverse the left side instead.
@@ -28,14 +37,20 @@ public class BasicMotion extends LinearOpMode {
         backLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
 
         waitForStart();
-
+        timer.reset();
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
             double y = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
             double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
-            double rx = gamepad1.right_stick_x;
 
+            if(timer.milliseconds()<=ratio) {
+
+//            double rx = gamepad1.right_stick_x;
+              rx= 1;
+            }else{
+                rx = 0;
+            }
             // Denominator is the largest motor power (absolute value) or 1
             // This ensures all the powers maintain the same ratio,
             // but only if at least one is out of the range [-1, 1]
@@ -49,6 +64,10 @@ public class BasicMotion extends LinearOpMode {
             backLeftMotor.setPower(backLeftPower);
             frontRightMotor.setPower(frontRightPower);
             backRightMotor.setPower(backRightPower);
+            telemetry.addData("left stick data ", -y);
+            telemetry.addData("right stick data ", rx);
+            telemetry.addData("timer secs ", timer.seconds());
+            telemetry.update();
         }
     }
 }
